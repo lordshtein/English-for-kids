416534df49ec1746b1289cd4e66c146f4769e12a
lordshtein <lordshtein@gmail.com>
2020-04-13 21:30:55 +0300

    feat: add side mune with animation

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,24 +3,40 @@ import cards from './cards';
 const Menu = {
   elements: {
     wrapper: null,
-    menuLayout: null,
+    menuList: null,
   },
 
   init() {
     this.elements.wrapper = document.createElement('div');
-    this.elements.menuLayout = document.createElement('ul');
+    this.elements.menuList = document.createElement('ul');
 
-    this.elements.wrapper.classList.add('menu-layout');
-    this.elements.keyboardLayout.appendChild(this.createLayout());
+    this.elements.wrapper.classList.add('menu-wrapper');
+    this.elements.menuList.classList.add('menu-list');
+    this.elements.menuList.appendChild(this.createMenu());
 
-    document.body.appendChild(this.elements.textarea);
-    document.body.appendChild(this.elements.description);
-    this.elements.wrapper.appendChild(this.elements.menuLayout);
-    document.body.appendChild(this.elements.wrapper);
+    this.elements.wrapper.appendChild(this.elements.menuList);
+    document.querySelector('.header').appendChild(this.elements.wrapper);
+  },
 
+  createMenu() {
+    const fragment = document.createDocumentFragment();
+    cards[0].forEach((e) => {
+      const menuItem = document.createElement('li');
+      menuItem.classList.add('menu__item');
+      menuItem.innerText = e;
+      fragment.appendChild(menuItem);
+    });
+    return fragment;
   },
 };
 
 window.addEventListener('DOMContentLoaded', () => {
   Menu.init();
 });
+
+const menuClick = () => {
+  document.querySelector('.header-menu').classList.toggle('menu__item_active');
+  document.querySelector('.menu-wrapper').classList.toggle('menu-wrapper_active');
+};
+
+document.querySelector('.header-menu').addEventListener('click', menuClick);
